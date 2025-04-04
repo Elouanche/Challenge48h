4457cc6ed09f65c9e01a6bba95b78e3fff1e4d56
Elouan CHÉDALLEUX <144027501+Elouanche@users.noreply.github.com>
2025-04-04 03:25:19 +0200

    Update MainLayout.jsx

--- challenge48withLaravel/resources/js/Layouts/MainLayout.jsx
+++ challenge48withLaravel/resources/js/Layouts/MainLayout.jsx
@@ -1,12 +1,12 @@
-import { Head, router } from '@inertiajs/react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import { useEffect } from 'react';
+import { Head, router } from "@inertiajs/react";
+import Header from "@/Components/Header";
+import Footer from "@/Components/Footer";
+import { useEffect } from "react";
 
 export default function MainLayout({ auth, children, title = null }) {
     useEffect(() => {
         if (!auth?.user) {
-            router.visit('/login');
+            router.visit("/login");
         }
     }, [auth]);
 
@@ -18,14 +18,13 @@ export default function MainLayout({ auth, children, title = null }) {
             <main className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            {children}
-                        </div>
+                        <div className="p-6 text-gray-900">{children}</div>
                     </div>
                 </div>
             </main>
-
-            <Footer />
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 }
