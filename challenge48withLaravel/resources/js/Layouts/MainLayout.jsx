bb8628fcc38add38344b29584c507d4bfc44b3c4
Elouan CHÉDALLEUX <144027501+Elouanche@users.noreply.github.com>
2025-04-04 03:28:26 +0200

    Update MainLayout.jsx

--- challenge48withLaravel/resources/js/Layouts/MainLayout.jsx
+++ challenge48withLaravel/resources/js/Layouts/MainLayout.jsx
@@ -11,18 +11,18 @@ export default function MainLayout({ auth, children, title = null }) {
     }, [auth]);
 
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen flex flex-col bg-gray-100">
             <Head title={title} />
             <Header auth={auth} />
 
-            <main className="py-12">
+            <main className="flex-grow py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">{children}</div>
                     </div>
                 </div>
             </main>
-            <footer>
+            <footer className="bg-gray-100">
                 <Footer />
             </footer>
         </div>
